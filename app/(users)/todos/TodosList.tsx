02cd2a0da87700e1adc3b9c43a07bbfaff259755
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-23 12:31:15 +0300

    created specific folders for components and implemented Suspence

--- app/(users)/todos/TodosList.tsx
+++ app/(users)/todos/TodosList.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
-import { Todo } from '../../typings'
+import { Todo } from '../../../typings'
 
 const fetchTodos = async () => {
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000
+  await new Promise((resolve) => setTimeout(resolve, timeout))
   const res = await fetch('https://jsonplaceholder.typicode.com/todos/')
   const todos: Todo[] = await res.json()
   return todos
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import TodosList from './(users)/todos/TodosList'
 
 function Home() {
-  return <div>I am the homepage</div>
+  return (
+    <div className="flex flex-col spacy-y-10">
+      <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <p className="text-blue-500">Loading the Shopping Trolley...</p>
+        }
+      >
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  )
 }
 
 export default Home
